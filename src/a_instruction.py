c76336b3ada7ed9b647a392ee95db1077de53a9d
renan <renancunhafonseca@gmail.com>
2019-12-25 18:14:56 -0300

    add support for pre-defined values in the @ instruction

--- src/a_instruction.py
+++ src/a_instruction.py
@@ -1,4 +1,12 @@
 
+pre_defined_symbols = {"SCREEN": "16384",
+                       "KBD": "24576",
+                       "SP": "0",
+                       "LCL": "1",
+                       "ARG": "2",
+                       "THIS": "3",
+                       "THAT": "4"}
+
 
 def is_a_instruction(string: str) -> bool:
     if string.startswith("@"):
@@ -7,12 +15,16 @@ def is_a_instruction(string: str) -> bool:
 
 
 def convert_a(string: str) -> str:
-    decimal_number = int(string[1:])
+    string = string[1:]
+    if not string.isdigit():
+        if string[0] == "R":
+            string = string[1:]
+        else:
+            string = pre_defined_symbols[string]
+    decimal_number = int(string)
     bin_number = bin(decimal_number)
     bin_str = str(bin_number)[2:]
     size = 15
     zeros = "0"*(size-len(bin_str))
     bin_str = zeros+bin_str
     return "0" + bin_str
-
-
