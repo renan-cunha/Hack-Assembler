ec5d5cf3ad0998682db217ec07eff8210e1dbdb0
renan <renancunhafonseca@gmail.com>
2019-12-25 15:52:34 -0300

    add method get_comp

--- src/c_instruction.py
+++ src/c_instruction.py
@@ -8,4 +8,19 @@ def get_dest(string: str) -> str:
     else:
         return string[:equal_index]
 
+
+def get_comp(string: str) -> str:
+    equal_index = string.find("=")
+    jump_index = string.find(";")
+    cond1 = equal_index != -1
+    cond2 = jump_index != -1
+    if not cond1 and not cond2:
+        return string
+    elif not cond1 and cond2:
+        return string[:jump_index]
+    elif cond1 and not cond2:
+        return string[equal_index+1:]
+    else:
+        return string[equal_index+1:jump_index]
+
 #def get_c_instruction_fields(string: str) -> Dict[str, str]:
--- tests/test_c_instruction.py
+++ tests/test_c_instruction.py
@@ -1,5 +1,5 @@
 import pytest
-from src.c_instruction import get_dest
+from src.c_instruction import get_dest, get_comp
 
 
 get_dest_data = [
@@ -11,3 +11,15 @@ get_dest_data = [
 @pytest.mark.parametrize("input,expected", get_dest_data)
 def test_get_dest(input, expected):
     assert get_dest(input) == expected
+
+
+get_comp_data = [
+    ("MD=D+1", "D+1"),
+    ("D+1",  "D+1"),
+    ("D;JMP",  "D"),
+    ("MD=D+1;JMP", "D+1"),
+]
+
+@pytest.mark.parametrize("input,expected", get_comp_data)
+def test_get_comp(input, expected):
+    assert get_comp(input) == expected
